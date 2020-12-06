156d9636e6086d621d246de06549a167222fe24f
Naph <nightcore.raven07@gmail.com>
2020-12-06 21:48:39 +0700

    chore: initial

---
+++ src/com/nabil/logincli/LoginCLI.java
@@ -0,0 +1,17 @@
+package com.nabil.logincli;
+/**
+ * A CLI-based login system
+ * @author klerikdust
+ */
+public class LoginCLI {
+
+	/**
+	 * Main workflow
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module com.nabil.logincli {
+}
